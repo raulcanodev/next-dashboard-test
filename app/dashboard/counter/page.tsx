b7b33f50ddc33e57f85c5ff836460664d2ec32e4
Raul M <118123543+pillow3@users.noreply.github.com>
2024-06-17 21:05:11 +0100

    first commit

---
+++ app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title:"Counter",
+  description:"A simple counter page",
+}
+
+export default function CounterPage() {
+  
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <span>Count</span>
+
+        <CartCounter value={20} />
+
+      </div>
+    </>
+  );
+}
