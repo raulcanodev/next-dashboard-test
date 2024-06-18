8045435a56458586fef0b7ad76ef62243cc78082
Raul M <118123543+pillow3@users.noreply.github.com>
2024-06-18 21:42:02 +0100

    priority false

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,16 +1,25 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from 'next/image'
+
 const getPokemons = async(limit = 20, offset= 0) =>{
-  const data = await fetch(
+  const data:PokemonsResponse = await fetch(
 		`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then(res => res.json())
   
-  return data;
+  const pokemons = data.results.map(pokemon=> ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name
+  }))
+
+  return pokemons;
 }
 
 export default async function PokemonsPage() {
-  const pokemons = await getPokemons();
+  const pokemons = await getPokemons(100);
   return (
-    <div>
-      {JSON.stringify(pokemons)}
-    </div>
-  )
+		<div className="flex flex-col">
+      <span className="text-5xl my-2">Pokemons list <small>Static</small></span>
+      <PokemonGrid pokemons={pokemons}/>
+		</div>
+  );
 }
---
+++ app/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,17 @@
+import { SimplePokemon } from "@/app/pokemons";
+import { PokemonCard } from "./PokemonCard";
+
+interface Props{
+  pokemons: SimplePokemon[];
+}
+
+export const PokemonGrid = ({pokemons}: Props) => {
+  return (
+		<div className="flex flex-wrap gap-10 items-center justify-center">
+			{pokemons.map((pokemon) => (
+				<PokemonCard pokemon={pokemon} key={pokemon.id}/>
+				
+			))}
+		</div>
+  );
+}
